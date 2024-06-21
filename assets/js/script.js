f30059c8bb334907ab4d7576d66aa20d2c579e27
Dan Feng <d.feng@hotmail.com>
2024-06-21 19:05:01 +0000

    Create funtion to display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You've clicked submit");
             } else {
                 let gameType = this.getAttribute("date-type");
-                alert(`You've clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    // Display default addition game when the webpage is initially loaded
+    runGame("addition");
 })
 
-function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+    // Create two random numbers between 1(inclusive) and 25(inclusive)
+    let num1 = Math.ceil(Math.random() * 25);
+    let num2 = Math.ceil(Math.random() * 25);
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -35,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "+";
 }
 
 function displaySubtractQuestion() {
