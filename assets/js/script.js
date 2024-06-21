9858a72b68b02bc4feb1703bd6dbde1e2cc234e7
Dan Feng <d.feng@hotmail.com>
2024-06-21 16:40:05 +0000

    Add code to generate two random number for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listener to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("date-type") === "submit") {
+                alert("You've clicked submit");
+            } else {
+                let gameType = this.getAttribute("date-type");
+                alert(`You've clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
